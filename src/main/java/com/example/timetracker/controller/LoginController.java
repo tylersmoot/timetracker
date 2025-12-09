04d2784a7ac45e7a9b56e8720b5b999cd4806ae8
tylersmoot <tylercsmoot8@gmail.com>
2025-12-09 12:44:42 -0600

    started initial file structure, created login and dashboard front end pages, tested get and post route for login page, tested data persistence to dashboard

--- src/main/java/com/example/timetracker/controller/LoginController.java
+++ src/main/java/com/example/timetracker/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.example.timetracker.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
@@ -13,9 +14,10 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam("email") String email, @RequestParam("password") String password) {
+    public String login(RedirectAttributes redirectAttributes, @RequestParam("email") String email, @RequestParam("password") String password) {
         System.out.println("Email: " + email + " " + "Pass hash: " + password);
-        return "dashboard";
+        redirectAttributes.addFlashAttribute("email", email);
+        return "redirect:/dashboard";
     }
 
 
